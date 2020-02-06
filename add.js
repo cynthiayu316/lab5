0f758b5447f89248c4a0c0c45b719f4df9e7cd96
cynthiayu316 <56209457+cynthiayu316@users.noreply.github.com>
2020-02-06 11:55:29 -0800

    Delete add.js

--- add.js
+++
@@ -1,9 +0,0 @@
-var data = require("../data.json");
-
-exports.addFriend = function(request, response) {    
-	var name = request.query.name;
-	var description = request.query.description;
-	var newFriend = {"name":name, "description": description, "imageURL":"https://picsum.photos/500/500"}
-	data.friends.push(newFriend)
-	response.render('index', data);
- }
